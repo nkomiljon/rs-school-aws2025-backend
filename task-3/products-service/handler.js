37bc5c92dea4b322f7eb472ad560e1ae21bed59f
Komiljon <komiljonnajmitdinov99@gmail.com>
2025-02-22 07:05:15 +0500

    init: created lambda functions

---
+++ task-3/products-service/handler.js
@@ -0,0 +1,27 @@
+const data = require("./data/products.json");
+exports.getProductList = async (event) => {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(data),
+  };
+};
+
+
+module.exports.getProductsById = async (event) => {
+  const { productId } = event.pathParameters;
+
+  const product = data.find((p) => p.id === productId);
+
+  if (!product) {
+    return {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Product not found" }),
+    };
+  }
+
+  return {
+    statusCode: 200,
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(product),
+  };
+};
