319e2e85a79728904c1d7136a5e3463f2b9a0ded
Komiljon <komiljonnajmitdinov99@gmail.com>
2025-03-01 20:48:40 +0500

    feat: changed products lambda

--- task-4/lib/task-4-stack.js
+++ task-4/lib/task-4-stack.js
@@ -12,74 +12,28 @@ class Task4Stack extends Stack {
   constructor(scope, id, props) {
     super(scope, id, props);
 
-    const stage = props?.stage || "dev";
+    const getProductsListFunction = new lambda.Function(
+      this,
+      "getProductsList",
+      {
+        runtime: lambda.Runtime.NODEJS_20_X,
+        handler: "getProductsList.handler", 
+        code: lambda.Code.fromAsset("lambda"), 
+      }
+    );
 
-    // Define Products Lambda functions
-    const productsFunction = new lambda.Function(this, `getProducts-${stage}`, {
-      runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "products.handler",
-      code: lambda.Code.fromAsset("lambda/products"),
-      timeout: Duration.seconds(10),
-      memorySize: 256,
+    // Create API Gateway
+    const api = new apigateway.RestApi(this, "ProductServiceAPI", {
+      restApiName: "Product Service",
+      description: "API for Product Service",
     });
 
-    const productsByIdFunction = new lambda.Function(
-      this,
-      `getProductsById-${stage}`,
-      {
-        runtime: lambda.Runtime.NODEJS_18_X,
-        handler: "products.handler",
-        code: lambda.Code.fromAsset("lambda/products"),
-        timeout: Duration.seconds(10),
-        memorySize: 256,
-      }
+    // Create /products endpoint
+    const productsResource = api.root.addResource("products");
+    productsResource.addMethod(
+      "GET",
+      new apigateway.LambdaIntegration(getProductsListFunction)
     );
-
-    // Create API Gateway and connect it to the Lambda function
-    const apiProducts = new apigateway.LambdaRestApi(
-      this,
-      `products-api-${stage}`,
-      {
-        restApiName: "Products Service",
-        description: "This is the products service API",
-        defaultCorsPreflightOptions: {
-          allowOrigins: apigateway.Cors.ALL_ORIGINS,
-          allowMethods: apigateway.Cors.ALL_METHODS,
-        },
-        deployOptions: {
-          stageName: stage, // Use the stage variable for the API Gateway stage
-        },
-        handler: productsFunction,
-        proxy: false, // Disable proxy to define specific routes
-      }
-    );
-
-    // Define an endpoint for GET /products
-    const productsResource = apiProducts.root.addResource("products");
-    productsResource.addMethod("GET"); // HTTP GET method
-
-    // Create API Gateway and connect it to the Lambda function
-    const apiProductsById = new apigateway.LambdaRestApi(
-      this,
-      `{id}`,
-      {
-        restApiName: "ProductsById Service",
-        description: "This is the products by id service API",
-        defaultCorsPreflightOptions: {
-          allowOrigins: apigateway.Cors.ALL_ORIGINS,
-          allowMethods: apigateway.Cors.ALL_METHODS,
-        },
-        deployOptions: {
-          stageName: stage, // Use the stage variable for the API Gateway stage
-        },
-        handler: productsByIdFunction,
-        proxy: false, // Disable proxy to define specific routes
-      }
-    );
-
-    // Define an endpoint for GET /products
-    const productsByIdResource = apiProductsById.root.addResource("products-by-id");
-    productsByIdResource.addMethod("GET"); // HTTP GET method
   }
 }
 
